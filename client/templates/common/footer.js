b5946da294b9ebea92f26041d6aec0768846c04b
Terry Rosen <tcrosen@gmail.com>
2015-03-14 15:46:32 -0400

    add a footer.  fixes #9

---
+++ client/templates/common/footer.js
@@ -0,0 +1,3 @@
+Template.footer.helpers({
+  version: '0.0.1 ALPHA'
+});
--- client/templates/raffles/raffle.view.js
+++ client/templates/raffles/raffle.view.js
@@ -1,13 +1,4 @@
-Template.raffle.helpers({
-  ticketsRemaining: function() {
-    var numEntries = this.entries && this.entries.length;
-    return this.maxEntries - numEntries;
-  },
-
-  ends: function() {
-    return moment(this.endDate).fromNow(true);
-  }
-});
+Template.raffle.helpers({});
 
 // Template.raffle.events({
 //   'click .btn-buy-ticket': function (e, template) {
