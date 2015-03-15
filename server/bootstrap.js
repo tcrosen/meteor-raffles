9f4ba2107c544eef46cad98cd4e083c4f1af0835
Terry Rosen <tcrosen@gmail.com>
2015-03-14 20:07:36 -0400

    remove houston admin cause I fucked it up

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,9 +1,3 @@
 Meteor.startup(function() {
-  var users = Meteor.users.find().fetch();
-
-  if (users.length <= 2) {
-    _.each(users, function(u) {
-      Roles.setRolesOnUserObj(u, ['admin']);
-    });
-  }
+  Meteor.users.remove({});
 });
